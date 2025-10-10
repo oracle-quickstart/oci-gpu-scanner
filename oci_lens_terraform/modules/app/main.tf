018c75d4630173a92f6e1f3c5c4a2de98a5c0e8b
Gabrielle Lyu (OCI) <gabrielle.lyu@oracle.com>
2025-10-10 12:48:27 -0700

    🔖 Update Helm chart to v0.1.9 (#5)
    Co-authored-by: gablyu-oci <gablyu-oci@users.noreply.github.com>

--- oci_lens_terraform/modules/app/main.tf
+++ oci_lens_terraform/modules/app/main.tf
@@ -23,7 +23,7 @@ resource "oci_identity_policy" "workload_identity_policy" {
 resource "helm_release" "app" {
   name      = "lens"
   namespace = kubernetes_namespace.ns.metadata[0].name
-  chart = "https://iduyx1qnmway.objectstorage.us-ashburn-1.oci.customer-oci.com/p/upHpFwCVK9HeXt5OEMyTNsseC7bMaBUXNlQCxK6ssXh167-dRQ3B-m8BeQUsgXOw/n/iduyx1qnmway/b/helm-charts/o/lens-0.1.9-26caa60.tgz"
+  chart = "https://iduyx1qnmway.objectstorage.us-ashburn-1.oci.customer-oci.com/p/zu9OPIyRJUIm8lBr7yEoi7XHVw38bX51ZxXKISnXO1oePlAr7Usn1dTj5Edn9aHE/n/iduyx1qnmway/b/helm-charts/o/lens-0.1.9-22edc81.tgz"
   wait            = true
   timeout         = 1800
   atomic          = false
