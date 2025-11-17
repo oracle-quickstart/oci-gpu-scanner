aa11e5b7aa6fe832b4b54210b89fa427b6cb4294
Gabrielle Lyu (OCI) <gabrielle.lyu@oracle.com>
2025-11-17 14:17:07 -0800

    🔖 Update Helm chart to v0.1.10 (#53)
    Co-authored-by: gablyu-oci <gablyu-oci@users.noreply.github.com>

--- oci_lens_terraform/modules/app/main.tf
+++ oci_lens_terraform/modules/app/main.tf
@@ -26,7 +26,7 @@ resource "oci_identity_policy" "workload_identity_policy" {
 resource "helm_release" "app" {
   name      = "lens"
   namespace = kubernetes_namespace.ns.metadata[0].name
-  chart = "https://oci-ai-incubations.github.io/corrino-lens-devops/lens-0.1.10-20251114-1852.tgz"
+  chart = "https://oci-ai-incubations.github.io/corrino-lens-devops/lens-0.1.10-20251117-2216.tgz"
   wait            = true
   timeout         = 1800
   atomic          = false
