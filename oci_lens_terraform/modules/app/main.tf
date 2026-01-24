bcf5cd516891141320a86de33be3ba79e14af5ee
gablyu-oci <gablyu-oci@users.noreply.github.com>
2026-01-24 06:22:23 +0000

    🔖 Update Helm chart to v0.1.16

--- oci_lens_terraform/modules/app/main.tf
+++ oci_lens_terraform/modules/app/main.tf
@@ -26,7 +26,7 @@ resource "oci_identity_policy" "workload_identity_policy" {
 resource "helm_release" "app" {
   name      = "lens"
   namespace = kubernetes_namespace.ns.metadata[0].name
-  chart = "https://oci-ai-incubations.github.io/corrino-lens-devops/lens-0.1.15-20260120-0942.tgz"
+  chart = "https://oci-ai-incubations.github.io/corrino-lens-devops/lens-0.1.16-20260124-0622.tgz"
   wait            = true
   timeout         = 1800
   atomic          = false
